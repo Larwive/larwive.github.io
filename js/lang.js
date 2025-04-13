65b9288d01507206667c880908eafd20a57ac102
Larwive <larwive@gmail.com>
2025-04-13 21:27:07 +0200

    Add functional first pages

---
+++ js/lang.js
@@ -0,0 +1,28 @@
+let currentLang = localStorage.getItem("lang") || "en";
+
+
+const toggleButton = document.getElementById("lang-toggle");
+
+async function updateText(lang) {
+    const response = await fetch(`lang/${lang}.json`);
+    const data = await response.json();
+
+    Object.keys(data).forEach(el => {
+        const element = document.getElementById(el);
+        if (element) {
+            element.textContent = data[el];
+        }
+    });
+
+    toggleButton.textContent = lang === "en" ? "EN" : "FR";
+    localStorage.setItem("lang", lang);
+}
+
+toggleButton.addEventListener("click", () => {
+    currentLang = currentLang === "en" ? "fr" : "en";
+    toggleButton.textContent = currentLang === "en" ? "EN" : "FR";
+    updateText(currentLang);
+});
+
+
+updateText(currentLang);
